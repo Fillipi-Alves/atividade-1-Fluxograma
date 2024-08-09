54de3a07ad3daf920be29536317d8d1ceedf5a5f
Fillipi alves <fillipialves3@gmail.com>
2024-08-09 15:01:36 -0300

    Add files via upload
    Python no Visual Studio

---
+++ aula.py
@@ -0,0 +1,22 @@
+
+  # EXERCICIO 1
+#for i in range(2, 11, 2):
+ # print(i)
+
+#EXERCÍCIO 2
+
+# soma= 50 
+# for numero in range (5,51,5):
+#     soma+= numero
+#     print(numero)
+
+# print('Soma dos multiplos de 5 até 50 = ',soma)
+
+# EXERCÍCIO 3
+
+# variavel = 10 
+# print ('O tipo da variavel é:', type(variavel))
+
+#EXERCÍCIO 4
+# nome = 'Fillipi Alves'
+# print(f'Bom dia',(nome),'Seja bem vindo ao programa')
